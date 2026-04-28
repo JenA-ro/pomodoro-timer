32db94d631f625920d501816a3ff828133628fa7
Potatore <181544040+JenA-ro@users.noreply.github.com>
2026-04-28 05:33:37 -0700

    Add countdown function for pomodoro timer

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const tabs = document.querySelectorAll("[data-tab-target]"); 
 const tabContent = document.querySelectorAll("[data-tab-content]");
 
+// time variables
+const minutes = 25;
+let time = minutes * 60;
+const countdown = document.getElementById('timer');
+
+//buttons
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const resetBtn = document.getElementById('reset-btn');
+
 // Function: allows tab (li tag) to be clicked and switch to that clicked tab 
 tabs.forEach(tab => {
   tab.addEventListener('click', () =>{
@@ -14,4 +24,22 @@ tabs.forEach(tab => {
     tab.classList.add('active')
     target.classList.add('active')
   })
-})
+});
+
+//later: make sure start button can only start on Pomodoro or Break tab
+//Function: countdown timer for Pomodoro
+function pomodoroCountdown() {
+  const minutes = Math.floor(time/60);
+
+  let seconds = time % 60;
+
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  countdown.innerHTML = `${minutes}:${seconds}`;
+  time--;
+};
+
+//Function: start button to start countdown timer
+startBtn.addEventListener('click', (event) => {
+  setInterval(pomodoroCountdown, 1000);
+});
